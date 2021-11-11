b2032574bf0e3e689aff943aced3c5929293b6b7
reza shakeri <r.shakeri9210@gmail.com>
2021-11-11 12:33:45 +0330

    [feat] publisher add controller added

--- src/publishers/controller/publishers/publishers.controller.ts
+++ src/publishers/controller/publishers/publishers.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PublishersService } from 'src/publishers/services/publishers/publishers.service';
 
 @Controller('publishers')
@@ -8,5 +8,12 @@ export class PublishersController {
   @Get()
   function() {
     return this.publisherService.getPublishers();
+  }
+  
+  @Post('add')
+  addPublisher(@Body() body) {
+    console.log(body);
+    
+    return this.publisherService.addPublisher(body);
   }
 }
--- src/publishers/services/publishers/publishers.service.ts
+++ src/publishers/services/publishers/publishers.service.ts
@@ -9,4 +9,34 @@ export class PublishersService {
     const res = await this.dbService.executeQuery('select  * from publisher');
     return res;
   }
+
+  async addPublisher(body: any) {
+    console.log(`select publisher_add(
+      "${body.last_name}",
+      "${body.first_name}",
+      "${body.email}",
+      "${body.province_state}",
+      "${body.city_town}",
+      "${body.street}",
+      "${body.alley}",
+      ${body.plaque_number},
+      ${body.floor_number},
+      ${body.unit_number},
+      "${body.zipcode}")`);
+    const res = await this.dbService.executeQuery(
+      `select publisher_add(
+        '${body.first_name}',
+        '${body.last_name}',
+        '${body.email}',
+        '${body.province_state}',
+        '${body.city_town}',
+        '${body.street}',
+        '${body.alley}',
+        ${body.plaque_number},
+        ${body.floor_number},
+        ${body.unit_number},
+        '${body.zipcode}')`,
+    );
+    return res;
+  }
 }
